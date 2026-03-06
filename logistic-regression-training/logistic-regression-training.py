650e75a742e8ac58aa2ac90aff6660a3f684d0eb
LamNguyen <lamnguyenduc0118@gmail.com>
2026-03-07 00:09:17 +0700

    Update solution for logistic-regression-training

--- logistic-regression-training/logistic-regression-training.py
+++ logistic-regression-training/logistic-regression-training.py
@@ -15,26 +15,16 @@ def train_logistic_regression(X, y, lr=0.1, steps=1000):
     n = len(y)
     
     for s in range(steps):
-        # z = np.array([X[i].dot(w) for i in range(n)]) + b
         z = w.dot(X.T) + b
-        print(z.shape)
         
         grad_z = _sigmoid(z) - y
-        # print(f"{grad_z.shape}, {X.shape}")
         
-        # grad_w = (grad_z * X).sum() * 1.0 / n
-        # grad_w = X.T.dot(grad_z) * 1.0 / n
         grad_w = grad_z.dot(X) / n
         grad_b = grad_z.sum() / n
-        # print(grad_w.shape)
-        # print(grad_b)
 
         w -= lr * grad_w
         b -= lr * grad_b
 
-    print("Done")
-    print(w.shape)
-    print(b.shape)
     
     return (w, b)
     pass
